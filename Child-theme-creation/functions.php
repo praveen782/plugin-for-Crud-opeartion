e44dcc36863637fd5d9b474593e90655cc76fe66
Praveen Kumar <42271169+praveen782@users.noreply.github.com>
2021-03-12 12:28:29 +0530

    Functions.php
    enqueue the style.css file into wordpress

---
+++ Child-theme-creation/functions.php
@@ -0,0 +1,50 @@
+<?php
+if ( !defined( 'ABSPATH' ) ) exit;
+
+if ( !function_exists( 'hestia_child_parent_css' ) ):
+    function hestia_child_parent_css() {
+        wp_enqueue_style( 'hestia_child_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'bootstrap' ) );
+	if( is_rtl() ) {
+		wp_enqueue_style( 'hestia_child_parent_rtl', trailingslashit( get_template_directory_uri() ) . 'style-rtl.css', array( 'bootstrap' ) );
+	}
+
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'hestia_child_parent_css', 10 );
+
+/**
+ * Import options from Hestia
+ *
+ * @since 1.0.0
+ */
+function hestia_child_get_lite_options() {
+	$hestia_mods = get_option( 'theme_mods_hestia' );
+	if ( ! empty( $hestia_mods ) ) {
+		foreach ( $hestia_mods as $hestia_mod_k => $hestia_mod_v ) {
+			set_theme_mod( $hestia_mod_k, $hestia_mod_v );
+		}
+	}
+}
+add_action( 'after_switch_theme', 'hestia_child_get_lite_options' );
+
+// function that runs when shortcode is called
+
+
+function my_enqueue() {
+
+    wp_enqueue_script( 'ajax-script', get_template_directory_uri() . '/js/my-ajax-script.js', array('jquery') );
+
+    wp_localize_script( 'ajax-script', 'my_ajax_object',    array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+}
+add_action( 'wp_enqueue_scripts', 'my_enqueue' );
+
+
+function wpb_custom_new_menu() {
+  register_nav_menu('my-custom-menu',__( 'My Custom Menu' ));
+}
+add_action( 'init', 'wpb_custom_new_menu' );
+
+function startOfMonth()
+{
+    return 1;
+}
